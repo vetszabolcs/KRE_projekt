b502ddf4acbfb36af9109478ebb3b2d2a2b8cb20
Szabolcs Vetési <70918904+vetszabolcs@users.noreply.github.com>
2022-04-06 13:52:06 +0200

    Update projekt3.30.r

--- codes/projekt3.30.r
+++ codes/projekt3.30.r
@@ -2,6 +2,7 @@ library(magrittr)
 library(ggplot2)
 library(plotly)
 library(dplyr)
+
 setwd("C:/Users/btk-sgt-006/Downloads")
 df <- read.csv("databases/twitter_2021_09_26_2022_03_10.csv")
 
@@ -23,16 +24,11 @@ unique(w_link$urls_expanded_url)
 
 
 top100_w_link <- w_link_uniq[order(w_link_uniq$favorite_count, decreasing = T), ][1:100,]
-
-unique(top100_w_link$urls_expanded_url)
-
-write.csv(top100_w_link, "top100_w_link.csv")
+write.csv(top100_w_link, "top100_w_link_unique.csv")
 
 
 t.df <- data.frame("date"= as.Date(unique(days)), "count"=as.numeric(table(days)))
-
 t.df <- t.df[order(t.df$nap), ]
-
 
 x <- ggplot(t.df, aes(date, count)) +
   geom_line(colour="blue", size=1.2)+
@@ -46,9 +42,9 @@ x <- ggplot(t.df, aes(date, count)) +
 ggplotly(x)
 
 
-
-# ?kezetmentes sz?vegek
-# legt?bb like-ot kapott linkes tweetek
-# emojik ?rtelmez?se
-# gyakoris?g id?ben
-# sentiment id?ben
+# TODO
+# ékezetmentes szövegek
+# legtöbb like-ot kapott linkes tweetek
+# emojik értelmezése
+# gyakoriság időben
+# sentiment időben
