140f07946729fc71dff2d2671d79f5e51242303c
Szabolcs Vetési <70918904+vetszabolcs@users.noreply.github.com>
2022-04-06 13:46:01 +0200

    Add files via upload

--- codes/projekt3.30.r
+++ codes/projekt3.30.r
@@ -1,6 +1,7 @@
 library(magrittr)
 library(ggplot2)
 library(plotly)
+library(dplyr)
 setwd("C:/Users/btk-sgt-006/Downloads")
 df <- read.csv("databases/twitter_2021_09_26_2022_03_10.csv")
 
@@ -12,9 +13,18 @@ days <- gsub(" .*", "", df$created_at)
 
 hist(table(days) %>% sort(decreasing = T))
 
-w_link <- df[!is.na(df$url),]
+w_link <- df[!is.na(df$urls_expanded_url) &
+            !grepl("twitter\\.", df$urls_expanded_url) &
+            grepl("\\.hu", df$urls_expanded_url) & 
+            !grepl("www\\.hu", df$urls_expanded_url),]
 
-top100_w_link <- w_link[order(w_link$favorite_count, decreasing = T), ][1:100,]
+w_link_uniq <- w_link %>% distinct(w_link$urls_expanded_url, .keep_all = T)
+unique(w_link$urls_expanded_url)
+
+
+top100_w_link <- w_link_uniq[order(w_link_uniq$favorite_count, decreasing = T), ][1:100,]
+
+unique(top100_w_link$urls_expanded_url)
 
 write.csv(top100_w_link, "top100_w_link.csv")
 
@@ -30,7 +40,9 @@ x <- ggplot(t.df, aes(date, count)) +
   scale_linetype_manual(name = "", values = "dotted")+
   ggtitle("Tweetek száma naponként")+
   xlab("Dátum") + ylab("Tweetek száma")+
-  theme(plot.title = element_text(hjust = 0.5, face = "bold"))
+  theme(plot.title = element_text(hjust = 0.5, face = "bold", size = 20),
+        axis.title = element_text(size = 15, face = "bold"),
+        axis.text = element_text(size=12))
 ggplotly(x)
 
 
